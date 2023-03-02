ca9b281b691fbbee5564694bec18439cb6e64062
Daniel Fernandez <dfernandezcode@gmail.com>
2023-03-02 23:42:56 +0100

    Update InputSample.js

--- src/components/InputSample/InputSample.js
+++ src/components/InputSample/InputSample.js
@@ -1,11 +1,22 @@
-
+import React from "react"
 import "./InputSample.css"
 
 const InputSample = () => {
+
+    console.log("paint inputSample");
+
+    //State to store input value.
+    const [name, setName] = React.useState("")
+
+    //useRef for DOM access
 return(
 
     <div className="input-sample">
-        <p>El valor</p>
+        <p>El valor atual es { name }</p>
+
+        <input onChange={ (event) => {
+            return setName(event.target.value);
+        } } type="text" placeholder="enter name"/>
     </div>
 )
 }
