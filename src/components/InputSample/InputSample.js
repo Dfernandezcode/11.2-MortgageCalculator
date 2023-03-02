4cd9c5d14aec3f6db2760b6d959c0ff3bc593a9a
Daniel Fernandez <dfernandezcode@gmail.com>
2023-03-02 23:56:28 +0100

    commit 2 - name field 11.2 autofocus implemented

--- src/components/InputSample/InputSample.js
+++ src/components/InputSample/InputSample.js
@@ -1,24 +1,47 @@
-import React from "react"
-import "./InputSample.css"
+import React from "react";
+import "./InputSample.css";
 
 const InputSample = () => {
+  console.log("paint inputSample");
 
-    console.log("paint inputSample");
+  //State to store input value.
+  const [name, setName] = React.useState("");
 
-    //State to store input value.
-    const [name, setName] = React.useState("")
+  //useRef for DOM access
+  const inputReference = React.useRef(null);
 
-    //useRef for DOM access
-return(
+  //Function for input-sample__btn - input value refresh
+  const getInputValue = () => {
+    console.log(inputReference.current);
+    console.log(inputReference.current.value);
+    setName(inputReference.current.value);
+  };
 
+  return (
     <div className="input-sample">
-        <p>El valor atual es { name }</p>
+      <p>El valor atual es {name}</p>
 
-        <input onChange={ (event) => {
-            return setName(event.target.value);
-        } } type="text" placeholder="enter name"/>
+      {/* <input
+        onChange={(event) => {
+          return setName(event.target.value);
+        }}
+        type="text"
+        placeholder="enter name"
+      /> */}
+
+      <input
+        autoFocus={true}
+        className="input-sample__input"
+        ref={inputReference}
+        type="text"
+        placeholder="enter name..."
+      />
+
+      <button className="input-sample__btn" onClick={getInputValue}>
+        Refresh Value
+      </button>
     </div>
-)
-}
+  );
+};
 
 export default InputSample;
